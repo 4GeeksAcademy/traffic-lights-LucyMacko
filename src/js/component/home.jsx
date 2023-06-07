067bea8e0ba7b21478e90b938871e29778774c9b
LucyMacko <118434024+LucyMacko@users.noreply.github.com>
2023-06-07 10:59:32 +0000

    Final version

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Home = () => {
+	const [selectedDiv, setSelectedDiv] = useState(null);
+
+	const handleClick = (divId) => {
+		setSelectedDiv(divId);
+	}
+
+	const existingClassesLight1 = 'red circle';
+	const existingClassesLight2 = 'orange circle';
+	const existingClassesLight3 = 'green circle';
+
+	const combinedClasses1 = `${existingClassesLight1} ${selectedDiv === 'div1' ? 'selected' : ''}`;
+	const combinedClasses2 = `${existingClassesLight2} ${selectedDiv === 'div2' ? 'selected' : ''}`;
+	const combinedClasses3 = `${existingClassesLight3} ${selectedDiv === 'div3' ? 'selected' : ''}`;
+
 	return (
-		<div className="text-center">
-			
+		<div>
+			<div className="container bg-dark" id="pole"></div>
+			<div className="container bg-dark pt-2" id="mainContainer">			
+				<div className={combinedClasses1} onClick={() => handleClick ('div1')}>🧍‍♀️</div>
+				<div className={combinedClasses2} onClick={() => handleClick ('div2')}></div>
+				<div className={combinedClasses3} onClick={() => handleClick ('div3')}>🏃‍♂️</div>			
+			</div>
 		</div>
 	);
 };
